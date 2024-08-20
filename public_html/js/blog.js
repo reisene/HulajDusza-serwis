a8247aa8c77d250d31e272ac49d6b3247c37c1e6
DraugSkoll <kprinny@gmail.com>
2024-08-20 06:55:49 +0200

    zmiana wygladu postu

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,17 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
     const postsContainer = document.getElementById('posts-container');
-    const posts = [
-        'post1.html',
-        'post2.html',
-        // Dodaj tutaj kolejne posty
+    const postPaths = [
+        'posts/post1.html',
+        'posts/post2.html',
+        // Inne posty
     ];
 
-    posts.forEach(post => {
-        fetch(`posts/${post}`)
+    postPaths.forEach(postPath => {
+        fetch(postPath)
             .then(response => response.text())
             .then(data => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(data, 'text/html');
+
+                const title = doc.querySelector('h2').outerHTML;
+                const date = doc.querySelector('.post-date').outerHTML;
+                const image = doc.querySelector('img').outerHTML;
+                const excerpt = doc.querySelector('section p').outerHTML;
+
                 const postElement = document.createElement('div');
-                postElement.innerHTML = data;
+                postElement.innerHTML = `${title}${date}${image}${excerpt} <button class="read-more">Czytaj więcej <i class="bi bi-file-earmark-post"></i></button>`;
+                
+                postElement.querySelector('.read-more').addEventListener('click', function(e) {
+                    e.preventDefault();
+                    postsContainer.innerHTML = doc.body.innerHTML; // Załadowanie całego posta
+                    postsContainer.innerHTML += '<button class="back-to-blog">Powrót do bloga <i class="fa-solid fa-arrow-rotate-left"></i></button>';
+                    document.querySelector('.back-to-blog').addEventListener('click', function() {
+                        location.reload(); // Powrót do strony bloga (odświeżenie)
+                    });
+                });
+
                 postsContainer.appendChild(postElement);
             })
             .catch(error => console.error('Error loading post:', error));
