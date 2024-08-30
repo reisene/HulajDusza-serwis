043751de7f637513b4a336c749269f5fe6ad5133
DraugSköll <kprinny@gmail.com>
2024-08-30 21:48:48 +0200

    gulpfile/components

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,15 @@ const fs = require('fs');
 const paths = {
   src: 'src/',
   partials: 'src/partials',
-  components: 'src/partials/components',
   dest: 'public_html/'
 };
-
-// Funkcja do przekształcania ścieżek include
-function resolveIncludePath(filePath) {
-  const componentsPath = path.join(__dirname, paths.components, filePath);
-  if (fs.existsSync(componentsPath)) {
-    return `components/${filePath}`;
-  }
-  return filePath;
-}
 
 gulp.task('html', function() {
   return gulp.src([path.join(paths.src, '**/*.html')]) // Przetwarza wszystkie pliki HTML w src i podfolderach
     .pipe(ignore.exclude('partials/**/*')) // Wyklucza folder partials z kopiowania
     .pipe(fileInclude({
       prefix: '@@',
-      basepath: path.join(__dirname, paths.partials), // Zaktualizowana ścieżka bazowa
-      filters: {
-        includeFile: function (filePath) {
-          return resolveIncludePath(filePath);
-        }
-      }
+      basepath: path.join(__dirname, paths.partials) // Ustawione na katalog partials, przeszukuje również podfoldery,
     }))
     .pipe(gulp.dest(paths.dest)); // Zapisuje przetworzone pliki HTML w folderze docelowym
 });
