d5adca49f18e5705f669dedb8439d773f5924e24
DraugSköll <kprinny@gmail.com>
2024-08-30 15:55:40 +0200

    gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,18 @@ const paths = {
 };
 
 gulp.task('html', function() {
-  return gulp.src([path.join(paths.src, '*.html')])
+  return gulp.src([path.join(paths.src, '**/*.html')]) // Przetwarza wszystkie pliki HTML w src i podfolderach
     .pipe(ignore.exclude('partials/**/*')) // Wyklucza folder partials z kopiowania
     .pipe(fileInclude({
       prefix: '@@',
-      basepath: '@file'
+      basepath: path.join(__dirname, 'src/partials') // Zaktualizowana ścieżka bazowa
     }))
     .pipe(gulp.dest(paths.dest)); // Zapisuje przetworzone pliki HTML w folderze docelowym
 });
 
 gulp.task('watch', function() {
   gulp.watch([
-    path.join(paths.src, '*.html'), // Monitoruje zmiany w folderze src
+    path.join(paths.src, '*.html', '**/*.html'), // Monitoruje zmiany w folderze src
     path.join(paths.partials, '**/*.html'), // Monitoruje zmiany w folderze partials
     ], gulp.series('html'));
 });
