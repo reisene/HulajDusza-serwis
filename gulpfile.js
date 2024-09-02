86c5b3f7ec5fdbba86680c04e8408d88124dee9f
DraugSköll <kprinny@gmail.com>
2024-09-02 22:28:39 +0200

    gulpfile for css

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,14 @@ const fileInclude = require('gulp-file-include');
 const replace = require('gulp-replace');
 const path = require('path');
 const ignore = require('gulp-ignore');
-const fs = require('fs');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const postcssPresetEnv = require('postcss-preset-env');
+const sourcemaps = require('gulp-sourcemaps');
 
 const paths = {
   src: 'src/',
+  css: 'src/css/**/*.css', // Ścieżka do plików CSS
   partials: 'src/partials',
   dest: 'public_html/'
 };
@@ -21,11 +25,24 @@ gulp.task('html', function() {
     .pipe(gulp.dest(paths.dest)); // Zapisuje przetworzone pliki HTML w folderze docelowym
 });
 
+// Zadanie do przetwarzania plików CSS
+gulp.task('css', function() {
+  return gulp.src(paths.css)
+    .pipe(sourcemaps.init())
+    .pipe(postcss([
+      autoprefixer(),
+      postcssPresetEnv({ stage: 3 })
+    ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(path.join(paths.dest, 'css')));
+});
+
 gulp.task('watch', function() {
   gulp.watch([
     path.join(paths.src, '*.html', '**/*.html'), // Monitoruje zmiany w folderze src
     path.join(paths.partials, '**/*.html'), // Monitoruje zmiany w folderze partials
     ], gulp.series('html'));
+  gulp.watch(paths.css, gulp.series('css')); // Monitoruje zmiany w plikach CSS
 });
 
-gulp.task('default', gulp.series('html'));
+gulp.task('default', gulp.series('html', 'css'));
