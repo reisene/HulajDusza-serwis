a84c259e4bf3e795092d65d97bd26b75f460c087
DraugSköll <kprinny@gmail.com>
2024-09-26 23:51:04 +0200

    Update send_email.js

--- public_html/js/send_email.js
+++ public_html/js/send_email.js
@@ -5,7 +5,7 @@
  * @param {string} token - The reCAPTCHA token obtained after user interaction.
  * @returns {void}
  */
-const form = document.getElementById("my-form");
+const form = $("#my-form");
 
 /**
  * @description Processes a form submission asynchronously, validating user input and
@@ -25,62 +25,23 @@ async function handleSubmit(event, token) {
     return;
   }
 
-  const notification = document.getElementById("notification");
-  const notificationMessage = document.getElementById("notification-message");
-  const formData = {
-    email: document.getElementById("email").value.trim(),
-    phone: document.getElementById("phone").value.replace(/\D/g, ''),
-    name: document.getElementById("name").value.trim(),
-    message: document.getElementById("message").value.trim()
-  };
+  // Get input data
+  const formData = getFormData();
 
-  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  const mobilePhonePattern = /^[0-9]{9}$/; // Polish mobile phone number pattern: 9 digits
-
-  if (!formData.email && !formData.phone) {
-    displayNotification("Proszę podać adres email lub numer telefonu.", 'error');
+  // Validate input data
+  if (!validateInput(formData)) {
     return;
   }
 
-  if (formData.email && !emailPattern.test(formData.email)) {
-    displayNotification("Proszę podać prawidłowy adres email.", 'error');
-    return;
-  }
-
-  if (formData.phone && !mobilePhonePattern.test(formData.phone)) {
-    displayNotification("Proszę podać prawidłowy numer telefonu (9 cyfr).", 'error');
-    return;
-  }
-
-  const submitButton = form.querySelector('button[type="submit"]');
-  if (submitButton) {
-    submitButton.disabled = true;
-  }
-
-  const uniqueID = `${Date.now()}${Math.floor(Math.random() * 1000000).toString(36)}`;
-
-  // Prepare data for sending
-  const data = new FormData();
-  data.append('name', formData.name);
-  data.append('email', formData.email);
-  data.append('phone', formData.phone);
-  data.append('message', formData.message);
-  data.append('g-recaptcha-response', token);
-  data.append('uniqueID', uniqueID);
-
-  // Send data to PHP
+  // Send data to server
   try {
-    const response = await fetch('/php/send_email.php', {
-      method: 'POST',
-      body: data,
-    });
-
+    const response = await sendFormData(formData, token);
     const result = await response.json();
 
     if (result.success) {
       displayNotification(result.message, 'success');
       setTimeout(() => {
-        form.reset(); // Reset the form after success
+        form[0].reset(); // Reset the form after success
       }, 5000); // Delaying reset after success message
     } else {
       displayNotification(result.message, 'error');
@@ -88,15 +49,79 @@ async function handleSubmit(event, token) {
   } catch (error) {
     console.error("Form submission error:", error);
     displayNotification("Ups! Wystąpił problem z przesłaniem formularza", 'error');
-  } finally {
-    // Re-enable the submit button
-    if (submitButton) {
-      submitButton.disabled = false;
-    }
   }
 }
 
-form.addEventListener("submit", async function(event) {
+/**
+ * Gets input data from the form.
+ *
+ * @returns {object} - The input data.
+ */
+function getFormData() {
+  const formData = {
+    email: $("#email").val().trim(),
+    phone: $("#phone").val().replace(/\D/g, ''),
+    name: $("#name").val().trim(),
+    message: $("#message").val().trim()
+  };
+
+  return formData;
+}
+
+/**
+ * Validates input data.
+ *
+ * @param {object} formData - The input data.
+ * @returns {boolean} - True if the input data is valid, false otherwise.
+ */
+function validateInput(formData) {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const mobilePhonePattern = /^[0-9]{9}$/; // Polish mobile phone number pattern: 9 digits
+
+  if (!formData.email && !formData.phone) {
+    displayNotification("Proszę podać adres email lub numer telefonu.", 'error');
+    return false;
+  }
+
+  if (formData.email && !emailPattern.test(formData.email )) {
+    displayNotification("Proszę podać prawidłowy adres email.", 'error');
+    return false;
+  }
+
+  if (formData.phone && !mobilePhonePattern.test(formData.phone)) {
+    displayNotification("Proszę podać prawidłowy numer telefonu (9 cyfr).", 'error');
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Sends form data to the server using AJAX.
+ *
+ * @param {object} formData - The input data.
+ * @param {string} token - The reCAPTCHA token.
+ * @returns {Promise} - The AJAX response.
+ */
+async function sendFormData(formData, token) {
+  const data = new FormData();
+  data.append('name', formData.name);
+  data.append('email', formData.email);
+  data.append('phone', formData.phone);
+  data.append('message', formData.message);
+  data.append('g-recaptcha-response', token);
+  data.append('uniqueID', `${Date.now()}${Math.floor(Math.random() * 1000000).toString(36)}`);
+
+  return $.ajax({
+    type: 'POST',
+    url: '/php/send_email.php',
+    data: data,
+    contentType: false,
+    processData: false
+  });
+}
+
+form.on("submit", async function(event) {
   event.preventDefault();
 
   try {
@@ -114,7 +139,7 @@ form.addEventListener("submit", async function(event) {
 });
 
 /**
- * @description Displays a notification to the user, indicating that it takes two
+ * Displays a notification to the user, indicating that it takes two
  * parameters: `message` and `type`. It adds the message and type to an existing HTML
  * element, notifies screen readers, and automatically hides the notification after
  * 5 seconds.
@@ -124,18 +149,34 @@ form.addEventListener("submit", async function(event) {
  * @param {string} type - Used to specify notification style.
  */
 function displayNotification(message, type) {
-  const notification = document.getElementById("notification");
-  const notificationMessage = document.getElementById("notification-message");
+  const notification = $("#notification");
+  const notificationMessage = $("#notification-message");
 
   if (notification && notificationMessage) {
-    notification.classList.add(type, 'show');
-    notificationMessage.innerHTML = message ;
+    notification.addClass(type, 'show');
+    notificationMessage.html(message);
 
     // Notify screen readers
-    notification.setAttribute('aria-live', 'assertive');
+    notification.attr('aria-live', 'assertive');
 
     setTimeout(() => {
-      notification.classList.remove('show', 'success', 'error');
+      notification.removeClass('show', 'success', 'error');
     }, 5000);
   }
 }
+
+// Formats phone input.
+$("#phone").on('input', function() {
+  // Formats phone numbers.
+  var value = $(this).val().replace(/\D/g, ''); // Remove all non-digit characters
+  var formattedValue = '';
+
+  for (var i = 0; i < value.length; i++) {
+    if (i > 0 && i % 3 === 0) {
+      formattedValue += ' ';
+    }
+    formattedValue += value[i];
+  }
+
+  $(this).val(formattedValue);
+});
