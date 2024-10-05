909c6cb467943be4c488aed68ec6ed2a96b8fce8
DraugSköll <kprinny@gmail.com>
2024-10-05 20:49:46 +0200

    updated babel

--- src/js/modules/phone_format.js
+++ src/js/modules/phone_format.js
@@ -1,24 +1,23 @@
-/**
- * @description Formats phone numbers input into a text field with a specific format,
- * separating digits every three characters with spaces. It achieves this by listening
- * for input changes and modifying the formatted value accordingly.
- */
 function phoneFormatter() {
   // Formats phone input.
-  $('#phone').on('input', function phoneInput() {
+  const phoneInput = document.getElementById('phone');
+
+  phoneInput.addEventListener('input', () => {
     // Formats phone numbers as user types.
-    var value = $(this).val().replace(/\D/g, ''); // Remove all non-digit characters
+    const value = phoneInput.value.replace(/\D/g, ''); // Remove all non-digit characters
     if (value.length === 9) {
-      var formattedValue = '';
-      for (var i = 0; i < value.length; i++) {
+
+      let formattedValue = '';
+      for (let i = 0; i < value.length; i++) {
         if (i > 0 && i % 3 === 0) {
           formattedValue += ' ';
         }
         formattedValue += value[i];
       }
-      $(this).val(formattedValue);
+      phoneInput.value = formattedValue;
     }
   });
-};
+
+}
 
 export default phoneFormatter;
