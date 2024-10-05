e1710f9ac3fd98af895001312dcd50ac69abce56
GitHub Action <action@github.com>
2024-10-05 21:29:00 +0000

    Automated Gulp build changes

--- public_html/js/modules/post-paths.js
+++ public_html/js/modules/post-paths.js
@@ -1,7 +1,1 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var _default = exports.default = ['/posts/post1.html', '/posts/post2.html'];
+export default["/posts/post1.html","/posts/post2.html"];
